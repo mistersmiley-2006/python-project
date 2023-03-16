26a4bdcb1d7cbedf4bc8eabe483bc5a69443a35e
Clinxig <cyril.tijou@laposte.net>
2023-03-16 15:01:07 +0100

    tetetee

--- encoder.py
+++ encoder.py
@@ -1,2 +1,6 @@
-# resalut
-# salut
+#recuperer le pixel en binaire
+
+
+#recuperer le code ascii qui correspond à cet indice 
+
+#si le dernier bit du pixel est le bon ne rien faire sinon changer le bit
