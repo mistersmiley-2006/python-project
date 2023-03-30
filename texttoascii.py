cf5679de0dd2a29bf92dd86d660efc9462f24504
mistersmiley-2006 <aeronef.interstellaire@gmail.comgit config --global user.name mistersmiley-2006git config --global user.email aeronef.interstellaire@gmail.com>
2023-03-30 14:05:07 +0200

    ETHAN

--- texttoascii.py
+++ texttoascii.py
@@ -1,2 +1,19 @@
 # ETHAN
-def phrase_binn()
+txt="le petit cheval dans la prairie"
+
+
+def transfo (txt):
+    '''
+    '''
+    long1=0
+    long=len(txt)
+    long1=bin(long)
+    temp1=0
+    list=[]
+    list.append(long1)
+    for element in txt :
+        temp1=ord(element)
+        temp1=bin(temp1)
+        list.append(temp1)
+        temp1=0
+    return list
