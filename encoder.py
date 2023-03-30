a76a12fd2758dcfef6716a77c8173f7cbefabfb6
Clinxig <cyril.tijou@laposte.net>
2023-03-30 15:17:54 +0200

    2e beta cyril encoder

--- encoder.py
+++ encoder.py
@@ -2,21 +2,48 @@ from PIL import Image
 
 image = Image.open("image_test.bmp")
 
+def TailleImgSuffisante(img: list, textAscii:list) ->bool:
+    '''
+    Renvoie un booleen vrai si la taille de l'image est suffisante
+    pour coder le texte dedans.
+    img : l'image dans laquelle on veut mettre le texte
+    textAscii : le texte en binaire
+    '''
+    # calcule de la definition de l'image
+    l, h = img.size()
+    definition = l*h
+
+    if definition > len(textAscii): # verifie si la definition de l'image est suffisante
+        return True
+    return False
+
 def EncoderImage(img: list,textAscii:list)->list:
     '''
     Code le message dans l'image
     img : l'image a modifier
     textAscii : la liste contenant le message sous forme binaire
     '''
-    l, h = img.size 
-    i = 0
-    msgSize = len(textAscii)
-    for x in range(l):
-        for y in range(h):
-            if i >= msgSize:
-                return img
-            r, g, b = img.getpixel((x, y))
-            if textAscii[i] != bin(b)[-1:]:
-                bin(b)[-1:] = textAscii[i]
-            i += 1
-    return img
+    if TailleImgSuffisante(img, textAscii):
+
+        # definition de la taille de l'image et des coordonnée
+        l, h = img.size 
+        x, y = 16, 0
+
+        for i in range(len(textAscii)):
+            r, g, b = img.getpixel((x, y)) # on recupère la couleur
+            if textAscii[i] != bin(b)[-1:]: # on verifie si on a besoin de modifier le bleu
+                b -= 1 # -1 pour changer le bit de 1
+
+            img.putpixel((x, y), (r, g, b)) # on injecte la nouvelle couleur
+            
+            # conditions au cas ou on arrive au bout de la ligne de l'image
+            if x >= l:
+                x = 0
+                y += 1
+            else:
+                x +=1
+
+        return img
+    else:
+        print("Erreur : la taille de l'image est insuffisante pour coder le texte")
+        return None
